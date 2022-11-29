b17ad70654305904a05b4bd8dd93d617a43bf7f5
sonwGlint <cosm.de.c@gmail.com>
2022-11-29 17:52:16 +0800

    初始化R包的结构

---
+++ R/add.R
@@ -0,0 +1,15 @@
+#' Add number
+#'
+#' @param a Number one
+#' @param b Number two
+#'
+#' @return sum of a and b
+#' @export
+#'
+#' @examples
+#' a <- 10
+#' b <- 20
+#' sum(a, b)
+add <- function(a, b) {
+  sum(a, b)
+}
